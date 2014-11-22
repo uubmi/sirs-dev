6543bb92eb8571c9b4d277ce60212c85851981cd
bheale <bret.heale@hsc.utah.edu>
2014-11-22 14:01:12 -0700

    Everything Seems okay?
    Everything seems fine. Not clear what
    'lost some of the features Bret designed means'? The response to missing
    and SIRS criteria are as before, as far as I can tell....help?

---
+++ configureEMR.js
@@ -0,0 +1,6 @@
+//this file, if we were to implement a true pull system
+//would contain the code that would provide authentication and source of data for the CDS local service
+//also it would where the local EMR IT staff would decide how the CDSS result would incarnate itself
+//also it would be where the local staff would provide instructions for when the data pull should occur
+//for the project the services are called by the EMR when data changes
+//or Called by a timing function currently embedded in the EMR
